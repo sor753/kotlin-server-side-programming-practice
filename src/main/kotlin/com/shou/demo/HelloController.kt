46c201ed740c589c5b4cb714f327145cda251340
sk <sor753@gmail.com>
2026-07-03 20:02:41 +0000

    REST API の実装

--- src/main/kotlin/com/shou/demo/HelloController.kt
+++ src/main/kotlin/com/shou/demo/HelloController.kt
@@ -3,6 +3,12 @@ package com.shou.demo
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 
 @Controller
 class HelloController {
@@ -12,3 +18,25 @@ class HelloController {
         return "index"
     }
 }
+
+@RestController
+@RequestMapping("greeter")
+class GreeterController {
+    @GetMapping("/hello")
+    fun hello(@RequestParam("name") name: String): HelloResponse {
+        return HelloResponse("Hello, $name!")
+    }
+
+    @GetMapping("/hello/{name}")
+    fun helloPath(@PathVariable("name") name: String): HelloResponse {
+        return HelloResponse("Hello, $name!")
+    }
+
+    @PostMapping("/hello")
+    fun helloByPost(@RequestBody request: HelloRequest): HelloResponse {
+        return HelloResponse("Hello, ${request.name}!")
+    }
+}
+
+data class HelloResponse(val message: String)
+data class HelloRequest(val name: String)
