5ee6b733a0b860b764eedcce17a91a0348fc5d63
sk <sor753@gmail.com>
2026-07-06 20:29:44 +0000

    ドメインの追加

---
+++ src/main/kotlin/com/shou/demo/domain/rental/Rental.kt
@@ -0,0 +1,11 @@
+package com.shou.demo.domain.rental
+
+import java.time.LocalDateTime
+
+data class Rental(
+    val id: Long,
+    val bookId: Long,
+    val userId: Long,
+    val rentalDatetime: LocalDateTime,
+    val returnDeadline: LocalDateTime
+)
